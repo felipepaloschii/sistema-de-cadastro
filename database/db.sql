49d9364653ac5b2c576f33b52b51efa312a811c7
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-08-18 10:51:57 -0300

    add - conexao

--- database/db.sql
+++ database/db.sql
@@ -6,7 +6,10 @@ CREATE TABLE pratos (
     nome VARCHAR(100) NOT NULL,
     descricao TEXT,
     preco DECIMAL(10, 2) NOT NULL,
-    categoria VARCHAR(50)
+    categoria VARCHAR(50),
+    usuario_id INT NOT NULL,
+    
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
 CREATE TABLE usuarios (
