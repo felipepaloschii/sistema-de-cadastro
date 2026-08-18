5e654a878166f56dcfe6fa3774589cc5ce83fd8b
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-08-18 10:07:14 -0300

    add - sql usuarios

--- database/db.sql
+++ database/db.sql
@@ -6,5 +6,11 @@ CREATE TABLE pratos (
     nome VARCHAR(100) NOT NULL,
     descricao TEXT,
     preco DECIMAL(10, 2) NOT NULL,
-    categoria VARCHAR(50),
+    categoria VARCHAR(50)
+);
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
 );
