a8a0ec482c466a3fc16eb509cce9139b0654c803
João Arthur Pereira <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-08-18 10:53:24 -0300

    add - cadastro pratos

---
+++ public/cadastrar.php
@@ -0,0 +1,16 @@
+<?php
+
+include "../infra/conexao.php";
+
+$nome = $_POST["nome"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+$categoria = $_POST["categoria"];
+
+
+$sql = "INSERT INTO pratos (nome, descricao, preco, categoria) VALUES ('$nome', '$descricao', '$preco', '$categoria')";
+
+mysqli_query($conexao, $sql);
+
+header("Location: ../index.php");
+?>
